6c96e9036bfa1b58c3667c881c6ed5cc04588447
Farhan izzaz <farhanizzaz@gmail.com>
2023-05-11 15:07:39 +0700

    Update

---
+++ src/routes/+page.server.js
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import { writeFile } from 'fs/promises';
+import util from 'util';
+import excelToJson from 'convert-excel-to-json';
+import { Client, LocalAuth } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal'
+import { redirect } from '@sveltejs/kit';
+
+
+export const actions = {
+    upload: async ({ cookies, request, locals }) => {
+
+        const client = new Client({
+            authStrategy: new LocalAuth(),
+            // proxyAuthentication: { username: 'username', password: 'password' },
+            puppeteer: {
+                // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
+                headless: true
+            }
+        });
+
+        client.initialize();
+
+        client.on('loading_screen', (percent, message) => {
+            console.log('LOADING SCREEN', percent, message);
+        });
+    
+        client.on('qr', (qr) => {
+            qrcode.generate(qr, { small: true });
+        });
+
+        client.on('authenticated', () => {
+            console.log('AUTHENTICATED');
+        });
+        
+        client.on('auth_failure', msg => {
+            // Fired if session restore was unsuccessful
+            console.error('AUTHENTICATION FAILURE', msg);
+        });
+        
+    
+        
+        
+        const data = await request.formData();
+        const fileName = 'temp'
+        
+        const file = data.get('file');
+        const msg = data.get('msg');
+        await writeFile(`./files/${fileName}.xls`, file.stream());
+        
+        const json = excelToJson({
+            sourceFile: `./files/${fileName}.xls`
+        });
+
+        console.log(json.Results)
+
+        client.on('ready', () => {
+            console.log('Client is ready!');
+            json.Results.forEach((item, index) => {
+                const chatId = item.A.toString().substring(0) + "@c.us";
+                console.log(chatId + ' ' + msg)
+                client.sendMessage(chatId, msg);
+            });
+            fs.unlink(`./files/${fileName}.xls`, (err) => {
+                if (err) throw err //handle your error the way you want to;
+                console.log('path/file.txt was deleted');//or else the file will be deleted
+            });
+        });
+        
+
+        return { success: true };
+    }
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	let files;
+	let text;
+</script>
+
+<form action="?/upload" method="post" enctype="multipart/form-data">
+	<input
+		bind:files
+		type="file"
+		accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+		name="file"
+		required
+	/>
+	<br />
+	<br />
+	<textarea bind:value={text} name='msg' required/>
+	<br />
+	<br />
+	<button type="submit"> Kirim </button>
+</form>
