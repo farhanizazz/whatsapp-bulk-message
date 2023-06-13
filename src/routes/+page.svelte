4b1f5c88b11f634008748f319f1dd87c7b46db7e
Farhan izzaz <farhanizzaz@gmail.com>
2023-06-13 13:03:49 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,29 @@
 </script>
 
 <form action="?/upload" method="post" enctype="multipart/form-data">
+	<label for="file">List nomor telepon (xls/xlsx)<br/></label>
 	<input
 		bind:files
 		type="file"
 		accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 		name="file"
+		id="file"
 		required
+	/>
+	<br/>
+	<br/>
+	<label for="file">Gambar (opsional)<br/></label>
+	<input
+		bind:files
+		type="file"
+		accept="image/bmp, image/jpeg, image/x-png, image/png, image/gif"
+		name="image"
+		id="image"
 	/>
 	<br />
 	<br />
-	<textarea bind:value={text} name='msg' required/>
+	<label for="msg">Pesan<br/></label>
+	<textarea id="msg" bind:value={text} name='msg' required/>
 	<br />
 	<br />
 	<button type="submit"> Kirim </button>
